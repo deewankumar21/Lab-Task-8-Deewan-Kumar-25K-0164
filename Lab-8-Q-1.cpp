d8a5c559d3e3ab16071c955b2d551f5e469f6085
deewankumar21 <k250164@nu.edu.pk>
2025-10-23 09:23:33 +0500

    Add files via upload

---
+++ Lab-8-Q-1.cpp
@@ -0,0 +1,13 @@
+#include <stdio.h>
+int main(){
+	int i,j;
+	printf("Paramid Pattern\n");
+	for(i=1;i<6;i++){
+		printf("%d:",i);
+		for(j=1;j<6;j++){
+			printf(" %d  ",i*j);
+		}
+		printf(" \n");
+	}
+	return 0;
+}
